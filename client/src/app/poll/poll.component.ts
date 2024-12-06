64e4fa106578df44bd64994bb2f062663eba0569
Francis7575 <francis.albert.2009@gmail.com>
2024-12-06 15:09:46 -0800

    added loadPolls in poll component and succesfully render html in the frontend

--- client/src/app/poll/poll.component.ts
+++ client/src/app/poll/poll.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Poll } from '../poll.models';
+import { PollService } from './../poll.service';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-poll',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './poll.component.html',
-  styleUrl: './poll.component.css'
+  styleUrl: './poll.component.css',
 })
-export class PollComponent {
+export class PollComponent implements OnInit {
+  polls: Poll[] = [];
+  constructor(private pollService: PollService) {}
 
+  // loadPolls on mount
+  ngOnInit(): void {
+    this.loadPolls();
+  }
+
+  loadPolls() {
+    this.pollService.getPolls().subscribe({
+      next: (data) => {
+        this.polls = data;
+      },
+      error: (error) => {
+        console.error('Error fetching polls', error);
+      },
+    });
+  }
 }
